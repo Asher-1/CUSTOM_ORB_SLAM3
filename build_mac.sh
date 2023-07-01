867b7c5e87f00ed96ed6ccb9177de3429ea097e8
Dahai <ludahai19@163.com>
2023-07-02 00:52:41 +0800

    fix files missing issues with time starts and add native build options

--- build_mac.sh
+++ build_mac.sh
@@ -1,9 +1,22 @@
 echo "Configuring and building Thirdparty/DBoW2 ..."
 
+if [ -n "$1" ]; then 
+    if [ "$1" = "OFF" -o "$1" = "ON" ]; then
+        BUILD_NATIVE=$1
+    else
+        echo "Wrong parameter for -BUILD_NATIVE! Only support OFF or ON"
+        exit
+    fi
+else
+    BUILD_NATIVE=ON
+fi
+
+echo "Build all libraries with native: $BUILD_NATIVE"
+
 cd Thirdparty/DBoW2
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_NATIVE=$BUILD_NATIVE
 make -j
 
 cd ../../g2o
@@ -12,7 +25,7 @@ echo "Configuring and building Thirdparty/g2o ..."
 
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_NATIVE=$BUILD_NATIVE
 make -j
 
 cd ../../Sophus
@@ -36,5 +49,5 @@ echo "Configuring and building ORB_SLAM3 ..."
 
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_NATIVE=$BUILD_NATIVE
 make -j
--- build_ubuntu.sh
+++ build_ubuntu.sh
@@ -1,9 +1,22 @@
 echo "Configuring and building Thirdparty/DBoW2 ..."
 
+if [ -n "$1" ]; then 
+    if [ "$1" = "OFF" -o "$1" = "ON" ]; then
+        BUILD_NATIVE=$1
+    else
+        echo "Wrong parameter for -BUILD_NATIVE! Only support OFF or ON"
+        exit
+    fi
+else
+    BUILD_NATIVE=ON
+fi
+
+echo "Build all libraries with native: $BUILD_NATIVE"
+
 cd Thirdparty/DBoW2
 mkdir build_ubuntu
 cd build_ubuntu
-cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_NATIVE=$BUILD_NATIVE
 make -j 4
 
 cd ../../g2o
@@ -12,7 +25,7 @@ echo "Configuring and building Thirdparty/g2o ..."
 
 mkdir build_ubuntu
 cd build_ubuntu
-cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_NATIVE=$BUILD_NATIVE
 make -j 4
 
 cd ../../Sophus
@@ -36,5 +49,5 @@ echo "Configuring and building ORB_SLAM3 ..."
 
 mkdir build_ubuntu
 cd build_ubuntu
-cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_NATIVE=$BUILD_NATIVE
 make -j 4
